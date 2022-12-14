25ab43afc52537875d54ecca38a0ef06e14f4b2e
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2022-12-14 23:31:57 +0100

    remove themes and add more keymappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,13 +22,28 @@ vim.opt.termguicolors = true
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<Leader>ff', '<CMD>Telescope find_files<CR>')
 vim.keymap.set('n', '<Leader>gf', '<CMD>Telescope live_grep<CR>')
+
 vim.keymap.set('n', 'gd', '<Plug>(coc-definition)', { silent = true } )
 vim.keymap.set('n', 'gi', '<Plug>(coc-implementation)', { silent = true } )
 vim.keymap.set('n', 'gr', '<Plug>(coc-references)', { silent = true } )
 vim.keymap.set('n', '<Leader>rn', '<Plug>(coc-rename)', { silent = true } )
 vim.keymap.set('n', '<Leader>pf', '<Plug>(Prettier)', { silent = true } )
+
 vim.keymap.set('n', '<Leader>ng', '<CMD>Neogit<CR>')
 vim.keymap.set('n', '<Leader>bn', ':<C-u>call gitblame#echo()<CR>', { silent = true } )
+
+vim.keymap.set('n', '<Leader>nt', ':tabnew %<CR>', { silent = true } )
+vim.keymap.set('n', '<M-k>', ':tabnext<CR>', { silent = false } )
+vim.keymap.set('n', '<M-j>', ':tabprevious<CR>', { silent = false } )
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 
@@ -48,8 +63,6 @@ vim.g.coc_global_extensions = {
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use 'gruvbox-community/gruvbox'
-  use 'mhartington/oceanic-next'
   use { "catppuccin/nvim", as = "catppuccin" }
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
@@ -93,7 +106,7 @@ return require('packer').startup(function(use)
   }
   
   require("catppuccin").setup()
-
   vim.cmd('colorscheme catppuccin')
+
 end)
 
