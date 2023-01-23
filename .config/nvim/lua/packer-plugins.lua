5e2e2ccf176fe3170567e0c6a09042458c99effb
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-01-23 07:08:54 +0100

    migrate to full LSP

--- .config/nvim/lua/packer-plugins.lua
+++ .config/nvim/lua/packer-plugins.lua
@@ -1,12 +1,13 @@
 require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use { "catppuccin/nvim", as = "catppuccin" }
+  use { 'catppuccin/nvim', as = 'catppuccin' }
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-treesitter/nvim-treesitter'
   use 'zivyangll/git-blame.vim'
-  -- use {'neoclide/coc.nvim', branch = 'release', run = 'yarn install --frozen-lockfile'}
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
   use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
   use 'hrsh7th/nvim-cmp' -- autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- lsp autocompletion
@@ -14,7 +15,6 @@ require('packer').startup(function(use)
   use 'hrsh7th/cmp-path'
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
-
   use {'prettier/vim-prettier', run = 'yarn install --frozen-lockfile'}
   use {
     'TimUntersberger/neogit',
